5d7d012bbafc24abcb72dbfc63925a4e84cd1545
bunkerheadz1998 <bunkerheadz1998@protonmail.com>
2025-04-29 03:14:05 +0200

    Format

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,26 +1,20 @@
 import type { PageLoad } from './$types';
 import { ARENA_TOKEN, ARENA_CHANNEL } from '$env/static/private';
-import {
-	ArenaClient,
-	type ArenaChannelApi,
-	type GetChannelContentsApiResponse
-} from 'arena-ts';
+import { ArenaClient, type ArenaChannelApi, type GetChannelContentsApiResponse } from 'arena-ts';
 
 export const load: PageLoad = async () => {
-	const client = new ArenaClient({ token: ARENA_TOKEN });
+  const client = new ArenaClient({ token: ARENA_TOKEN });
 
-	const channel: ArenaChannelApi = await client
-		.channel(ARENA_CHANNEL)
+  const channel: ArenaChannelApi = await client.channel(ARENA_CHANNEL);
 
-	const { contents: posts }: GetChannelContentsApiResponse = await channel.contents()
+  const { contents: posts }: GetChannelContentsApiResponse = await channel.contents();
 
-	const lastSeen = posts
-		.map(({ updated_at }) => updated_at)
-		.sort((a, b) => new Date(b).getTime() - new Date(a).getTime())[0]
+  const lastSeen = posts
+    .map(({ updated_at }) => updated_at)
+    .sort((a, b) => new Date(b).getTime() - new Date(a).getTime())[0];
 
   return {
     posts,
-		lastSeen
+    lastSeen
   };
 };
-
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
-import { type ArenaChannelContents } from 'arena-ts';
+  import { type ArenaChannelContents } from 'arena-ts';
 
-	export let data;
+  export let data;
 
-	const posts: ArenaChannelContents = data.posts;
+  const posts: ArenaChannelContents = data.posts;
 </script>
 
 <main>
-	{#each posts as post}
-		<article>
-			{#if post.class == 'Image'}
-				<img src={post.image.display.url}/>
-			{:else if post.class == 'Video'}
-				<Video src={post.image.display.url}/>
-			{:else if post.class == 'Media'}
-				{@html post.embed.html}
-			{/if}
-		</article>
-	{/each}
+  {#each posts as post}
+    <article>
+      {#if post.class == 'Image'}
+        <img src={post.image.display.url} />
+      {:else if post.class == 'Video'}
+        <Video src={post.image.display.url} />
+      {:else if post.class == 'Media'}
+        {@html post.embed.html}
+      {/if}
+    </article>
+  {/each}
 </main>
 
 <style>
-	main {
-		max-width: 440px;
-	}
+  main {
+    max-width: 440px;
+  }
 
-	img {
-		min-width: 200px;
-		max-width: 100%;
-	}
+  img {
+    min-width: 200px;
+    max-width: 100%;
+  }
 </style>
